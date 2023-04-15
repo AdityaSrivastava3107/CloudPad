c17f85b041ae9149978b6881455910467830db03
Aditya Srivastava <kallakallz123@gmail.com>
2023-04-15 22:02:08 +0530

    Added data express data validator

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&directConnection=true&tls=false"
+const mongoURI = "mongodb://localhost:27017/CloudPad?readPreference=primary&directConnection=true&tls=false"
 const connectToMongo=async()=>{
     await mongoose.connect(mongoURI)
     console.log("Mongoose Connected");
