3adcda376d5b6e9415110d6904839fffa86d2769
Aditya Srivastava <kallakallz123@gmail.com>
2023-04-17 15:43:52 +0530

    Added password hashing and JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,51 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-const {body, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.post('/', [
-    body('name', 'Enter a valid name').isLength({min :3}),
-    body('email', 'Enter valid email').isLength({min :3}),
-    body('password', 'Password should be greater than 5 charecters').isLength({min :5})
-] , (req, res)=>{ 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+const JWT_SECRET = "ThisisCloudPad";
+//adding validation
+router.post('/createuser', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter valid email').isLength({ min: 3 }),
+  body('password', 'Password should be greater than 5 charecters').isLength({ min: 5 })
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  //checking for same email
+  try {
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ errors: "User with same email already exists" });
     }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=> {console.log(err)
-    res.json({error: 'Please enter a unique value for email', message: err.message})})
- 
-} )
+    //Password hashing and salt
+    const salt = await bcrypt.genSalt(10);
+    const secretPass = await bcrypt.hash(req.body.password, salt);
+    // new user
+    user = await User.create({
+      name: req.body.name,
+      password: secretPass,
+      email: req.body.email,
+    })
+    // authentication token
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+
+    res.json({authtoken})
+  }
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some unknown error occured");
+  }
+
+})
 module.exports = router
